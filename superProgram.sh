b4dbc412ff1867efb1d94131d6340bb91c975f16
julien.zanon <julien.zanon@docdoku.com>
2017-03-23 22:19:05 +0100

    Progam modified but I won't say how 41

--- superProgram.sh
+++ superProgram.sh
@@ -2,30 +2,3 @@
 STRING="Failure"
 echo $STRING
 
-# Useless comment 13
-# Useless comment 14
-# Useless comment 15
-# Useless comment 16
-# Useless comment 17
-# Useless comment 18
-# Useless comment 19
-# Useless comment 20
-# Useless comment 21
-# Useless comment 22
-# Useless comment 23
-# Useless comment 24
-# Useless comment 25
-# Useless comment 26
-# Useless comment 27
-# Useless comment 28
-# Useless comment 29
-# Useless comment 30
-# Useless comment 31
-# Useless comment 32
-# Useless comment 34
-# Useless comment 35
-# Useless comment 36
-# Useless comment 37
-# Useless comment 38
-# Useless comment 39
-# Useless comment 40
