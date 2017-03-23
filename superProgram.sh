64759f712937e97faae58eecadd90d5788ca058d
julien.zanon <julien.zanon@docdoku.com>
2017-03-23 21:35:46 +0100

    Progam modified but I won't say how 10

--- superProgram.sh
+++ superProgram.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 STRING="Success"
 echo $STRING
-# useless comment
-# Useless comment 0
-# Useless comment 1
-# Useless comment 2
-# Useless comment 3
-# Useless comment 4
-# Useless comment 5
-# Useless comment 6
-# Useless comment 7
-# Useless comment 8
-# Useless comment 9
+
